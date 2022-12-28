6057502f197f2cc4f0c37ceb2f3cd61e3b0edbd6
saddamarbaa <saddamarbaas@gamil.com>
2022-12-28 11:19:16 +0700

    chore: remove schema validation from optional filed

--- src/utils/schemaValidation/auth.ts
+++ src/utils/schemaValidation/auth.ts
@@ -1,19 +1,14 @@
 import * as Yup from 'yup';
 
 export const signupSchemaValidation = Yup.object().shape({
-  firstName: Yup.string()
-    .required('FirstName is required')
-    .min(3, 'FirstName must be at least 3 characters')
-    .max(10, 'FirstName must not exceed 10 characters'),
-  lastName: Yup.string()
-    .required('LastName is required')
-    .min(3, 'LastName must be at least 3 characters')
-    .max(10, 'LastName must not exceed 10 characters'),
-  familyName: Yup.string()
-    .nullable()
-    .transform((o, c) => (o === '' ? null : c))
-    .min(3, 'familyName must be at least 3 characters')
-    .max(10, 'familyName must not exceed 10 characters'),
+  name: Yup.string()
+    .required('Name is required')
+    .min(3, 'Name must be at least 3 characters')
+    .max(10, 'Name must not exceed 10 characters'),
+  surname: Yup.string()
+    .required('Surname is required')
+    .min(3, 'Surname must be at least 3 characters')
+    .max(10, 'Surname must not exceed 10 characters'),
   email: Yup.string().required('Email is required').email('Email is invalid'),
   password: Yup.string()
     .required('Password is required')
@@ -71,4 +66,28 @@ export const updatePasswordSchemaValidation = Yup.object().shape({
     .required('Confirm Password is required')
     .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
   acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required'),
+});
+
+export const updateUserSchemaValidation = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .min(3, 'Name must be at least 3 characters')
+    .max(10, 'Name must not exceed 10 characters'),
+  surname: Yup.string()
+    .required('Surname is required')
+    .min(3, 'Surname must be at least 3 characters')
+    .max(10, 'Surname must not exceed 10 characters'),
+  acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required'),
+  jobTitle: Yup.string()
+    .min(3, 'Job Title must be at least 3 characters')
+    .max(15, 'Job Title must not exceed 15 characters'),
+  companyName: Yup.string()
+    .min(3, 'Company Name cant be smaller than 3 characters')
+    .max(30, 'Company Name cant be greater than 30 characters'),
+  favoriteAnimal: Yup.string()
+    .min(3, 'Favorite Animal cant be smaller than 3 characters')
+    .max(15, 'Favorite Animal cant be greater than 15 characters'),
+  bio: Yup.string()
+    .min(10, 'Bio must be at least 10 characters')
+    .max(300, 'Bio must not exceed 100 characters'),
 });
